110b7f19bccb4bf5a7e3108d9b0df1b12cf3b241
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-04-17 16:34:02 +0600

    up

--- WeTogether/src/app/request/request.component.ts
+++ WeTogether/src/app/request/request.component.ts
@@ -10,10 +10,28 @@ export class RequestComponent implements OnInit {
 
   constructor(public service:ProfileService) { }
   userid:any=localStorage.getItem('UserId');
-
+  Name:any=localStorage.getItem('Name');
   ngOnInit(): void {
     this.service.FRequestGetByReceiverId();
     console.log(this.service.freqList);
+  }
+
+  acceptFriendRequest(Id:String,Rname:String,Requestid:String){
+    this.service.frequest.RequestId=Requestid;
+    this.service.frequest.ReceiverId=this.userid;
+    this.service.frequest.SenderId=Id;
+    this.service.frequest.ReceiverName=this.Name;
+    this.service.frequest.SenderName=Rname;
+    this.service.frequest.IsAccepted=true;
+    this.service.acceptFriendRequest().subscribe(
+      res=>{
+        console.log(res);
+        this.ngOnInit();
+      },
+      err=>{
+        console.log(err);
+      }
+    )
   }
 
 }
