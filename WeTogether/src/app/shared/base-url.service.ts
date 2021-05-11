75523b0aaa5624cd1dfd96aa0615bcaa97834667
Imran Hossain <57352521+imranalways@users.noreply.github.com>
2021-05-11 17:38:48 +0600

    Update base-url.service.ts

--- WeTogether/src/app/shared/base-url.service.ts
+++ WeTogether/src/app/shared/base-url.service.ts
@@ -8,5 +8,5 @@ export class BaseURLService {
   constructor() { }
 
   // readonly baseUrl = 'http://10.100.14.204:8091';
-    readonly baseUrl = 'http://10.100.14.204:8091';
+  readonly baseUrl = 'https://localhost:44388';
 }
